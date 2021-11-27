72988df091dc9a483405687d9e8c92abdf427c4a
israel_yaacobovich <77208779+Israel0545488592@users.noreply.github.com>
2021-11-27 12:05:10 +0200

    Delete TestNetBuilder.java
    needed to get rid of it temperrarly

--- TestNetBuilder.java
+++
@@ -1,9 +0,0 @@
-public class TestNet {
-
-    static String Path = "C:/Users/ישראל/DS/Algo_for_AI/Esighnment/alarm_net.xml";
-
-    public static void main(String[] args) throws Exception {
-        Net net = new Net(Path);
-        System.out.println(net);
-    }
-}
